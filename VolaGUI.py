fdfe7752dc799e52ca7be20f9606c63ad2600fff
GABuras <78371257+GABuras@users.noreply.github.com>
2023-11-06 20:48:03 -0600

    Created VolaGUI

---
+++ VolaGUI.py
@@ -0,0 +1,27 @@
+import sys
+
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("VolaGUI")
+
+        button = QPushButton("Press Me!")
+
+        self.setFixedSize(QSize(1400, 800))
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
