a99fc0fd4e20c250386e8ce1d2ef1cff4723b50f
Brennen Calato <bdcalato@gmail.com>
2023-11-10 12:30:34 -0600

    Table Filtering Implemented

--- TableTesting.py
+++ TableTesting.py
@@ -1,5 +1,5 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QSortFilterProxyModel
 import sys;
 import DataHandling
 
@@ -8,35 +8,56 @@ class Window(QtWidgets.QWidget):
         super().__init__()
         self.service = service
         self.setWindowTitle("Results")
-        self.resize(1250, 350)
+        self.resize(1275, 350)
         self.CreateTable()
         self.show()
+        self.filter: QSortFilterProxyModel
     
     def CreateTable(self):
-        self.table = QtWidgets.QTableWidget()
-        # self.table.setRowCount(5)
-        # self.table.setColumnCount(3)
         self.PopulateTable(self.service)
         self.vBox = QtWidgets.QVBoxLayout()
-        self.vBox.addWidget(self.table)
         self.setLayout(self.vBox)
+
+        searchfield = QtWidgets.QLineEdit()
+
+        self.filter_choice = QtWidgets.QComboBox()
+        for headers in self.data[self.service]["headers"]:
+            self.filter_choice.addItem(headers)
+
+        self.filter = QSortFilterProxyModel()
+        self.filter.setSourceModel(self.model)
+
+        self.filter_choice.activated.connect(self.set_filter)
+
+        searchfield.setStyleSheet("font-size: 20px; height: 35px;")
+        searchfield.textChanged.connect(self.filter.setFilterFixedString)
+
+        self.vBox.addWidget(self.filter_choice)
+        self.vBox.addWidget(searchfield)
+        table_view = QtWidgets.QTableView()
+        table_view.setModel(self.filter)
+        self.vBox.addWidget(table_view)
+
     def PopulateTable(self, service):
         # grabs the command data dictionary
-        data = DataHandling.command_data
+        self.data = DataHandling.command_data
         # pulls command specific column count
-        columns = data[service]["columns"]
+        columns = self.data[service]["columns"]
         #dynamically calculates the length of data in the file
-        rows = data["rows"](f"{service}.txt")
-        self.table.setRowCount(rows)
-        self.table.setColumnCount(columns)
-        self.table.setHorizontalHeaderLabels(data[service]["headers"])
+        rows = self.data["rows"](f"{service}.txt")
+        self.model = QtGui.QStandardItemModel(rows, columns)
+        self.model.setHorizontalHeaderLabels(self.data[service]["headers"])
 
         with open(f"./data/{service}.txt", "r") as file:
             for r in range(rows):
                 entry = file.readline()
                 column_entries = entry.split(",")
                 for c in range(columns):
-                    self.table.setItem(r, c, QtWidgets.QTableWidgetItem(str(column_entries[c])))
+                    item = QtGui.QStandardItem(str(column_entries[c]))
+                    self.model.setItem(r, c, item)
+    
+    def set_filter(self):
+        self.filter.setFilterKeyColumn(self.filter_choice.currentIndex())
 
 choice = input("Please type pslist or psscan to get results: ")
 app = QtWidgets.QApplication(sys.argv)
