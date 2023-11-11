034f33915a8e6cb664e1e5aedb33fbf15e386fe2
Brennen Calato <bdcalato@gmail.com>
2023-11-10 23:38:41 -0600

    Commented Results Layout for documentation and learning purposes

--- ResultTable.py
+++ ResultTable.py
@@ -1,6 +1,5 @@
-from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6 import QtGui, QtWidgets
 from PyQt6.QtCore import Qt, QSortFilterProxyModel
-import sys;
 import DataHandling
 
 class ResultWidget(QtWidgets.QWidget):
@@ -16,41 +15,49 @@ class ResultWidget(QtWidgets.QWidget):
     
     def CreateTable(self):
         self.PopulateTable(self.service)
-        self.vBox = QtWidgets.QVBoxLayout()
-        self.setLayout(self.vBox)
 
-        self.hBox = QtWidgets.QHBoxLayout()
-
+        # Creates a search widget using QLineEdit
         self.searchfield = QtWidgets.QLineEdit()
         self.searchfield.setFixedWidth(250)
         self.searchfield.setAlignment(Qt.AlignmentFlag.AlignLeft)
-
         self.searchfield.setPlaceholderText("Search...")
+        self.searchfield.setStyleSheet("font-size: 15px; height: 20px;")
 
+        # sets up the filter dropdown based on service headers
         self.filter_choice = QtWidgets.QComboBox()
         for headers in self.data[self.service]["headers"]:
             self.filter_choice.addItem(headers)
-        
-        self.searchfield.setAlignment(Qt.AlignmentFlag.AlignLeft)
-
+        # sets up filter based on the table model
+        # created in 'PopulateTable()' 
         self.filter = QSortFilterProxyModel()
         self.filter.setSourceModel(self.model)
-
+        # changes filter choice when something in drop down is selected
         self.filter_choice.activated.connect(self.set_filter)
         self.filter_choice.setFixedWidth(250)
 
-        self.searchfield.setStyleSheet("font-size: 15px; height: 20px;")
+        # Connects the filter and the search bar, filtering as text changes
         self.searchfield.textChanged.connect(self.filter.setFilterFixedString)
+        
+        # Established horizontal box for the filter and search widgets
+        # aligning it to the left side
+        self.hBox = QtWidgets.QHBoxLayout()
         self.hBox.addWidget(self.filter_choice)
         self.hBox.addWidget(self.searchfield)
         self.hBox.setAlignment(Qt.AlignmentFlag.AlignLeft)
+
+        # sets up a table view of the model
         self.table_view = QtWidgets.QTableView()
         self.table_view.setModel(self.filter)
 
+        # dynamically stretches the columns to make them fit the widget
         for c in range(self.data[self.service]["columns"]):
             self.table_view.horizontalHeader().setSectionResizeMode(c, 
                 QtWidgets.QHeaderView.ResizeMode.Stretch)
-
+        
+        # sets up the vertical box to hold the horizontal layout
+        # and the table view
+        self.vBox = QtWidgets.QVBoxLayout()
+        self.setLayout(self.vBox)
         self.vBox.setSpacing(0)
         #self.table_view.setMaximumHeight(175)
         self.vBox.addLayout(self.hBox)
@@ -64,9 +71,12 @@ class ResultWidget(QtWidgets.QWidget):
         columns = self.data[service]["columns"]
         #dynamically calculates the length of data in the file
         rows = self.data["rows"](f"{service}.txt")
+        # established model of data
         self.model = QtGui.QStandardItemModel(rows, columns)
+        # sets data model headers
         self.model.setHorizontalHeaderLabels(self.data[service]["headers"])
 
+        # loops through the file to populate the model
         with open(f"./data/{service}.txt", "r") as file:
             for r in range(rows):
                 entry = file.readline()
@@ -75,6 +85,7 @@ class ResultWidget(QtWidgets.QWidget):
                     item = QtGui.QStandardItem(str(column_entries[c]))
                     self.model.setItem(r, c, item)
     
+    # used to set the filter when changed
     def set_filter(self):
         self.filter.setFilterKeyColumn(self.filter_choice.currentIndex())
 
