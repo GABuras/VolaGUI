31d9056e5b78c6e4f2bf1911e137b2995888aab7
Taylor Scott <tayzscott@gmail.com>
2023-11-12 18:09:00 -0600

    add CommandDropdown.py

---
+++ CommandDropdown.py
@@ -0,0 +1,37 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QTreeWidget, QTreeWidgetItem
+from PyQt6.QtCore import pyqtSlot
+
+
+# command list (TODO: add 'list all')
+data = {"DLLs": ["dlldump", "dlllist"],
+        "Modules": ["moddump", "modules", "modscan"],
+        "Processes": ["pslist", "psscan", "pstree"],
+        "Registry": ["hivedump", "hivelist", "hivescan"]}
+
+app = QApplication([])
+
+tree = QTreeWidget()  # add parent here
+tree.setColumnCount(1)
+tree.setHeaderLabels(["Commands"])
+
+# onClick function for the commands
+@pyqtSlot(QTreeWidgetItem, int)
+def onItemClick(it, col):
+    print(f"The button {it.text(col)} has been pressed.")
+
+# add data to the tree
+commands = []
+for key, values in data.items():
+    command = QTreeWidgetItem([key])
+    for value in values:
+        ext = value.split(".")[-1]
+        child = QTreeWidgetItem([value, ext])
+        command.addChild(child)
+    commands.append(command)
+
+tree.insertTopLevelItems(0, commands)
+tree.itemClicked.connect(onItemClick)  # make menu items clickable
+
+tree.show()
+sys.exit(app.exec())
