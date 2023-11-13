a22a407872b51850412825a814ac9c73f9225bc2
Taylor Scott <tayzscott@gmail.com>
2023-11-12 20:36:06 -0600

    update CommandDropdown.py to widget

--- CommandDropdown.py
+++ CommandDropdown.py
@@ -1,43 +1,40 @@
-import sys
-from PyQt6.QtWidgets import QApplication, QTreeWidget, QTreeWidgetItem
+from PyQt6.QtWidgets import QTreeWidget, QTreeWidgetItem, QWidget
 from PyQt6.QtCore import pyqtSlot
 
 
-# command list (TODO: add 'list all')
-data = {"DLLs": ["dlldump", "dlllist"],
-        "Modules": ["moddump", "modules", "modscan"],
-        "Processes": ["pslist", "psscan", "pstree"],
-        "Registry": ["hivedump", "hivelist", "hivescan"]}
+class TreeWidget(QTreeWidget):
+    def __init__(self, parent: QWidget | None):
+        super().__init__(parent)
+        
+        tree = QTreeWidget()  # add parent here
+        tree.setColumnCount(1)
+        tree.setHeaderLabels(["Commands"])
 
-app = QApplication([])
+        # command list (TODO: add 'list all')
+        data = {"DLLs": ["dlldump", "dlllist"],
+                "Modules": ["moddump", "modules", "modscan"],
+                "Processes": ["pslist", "psscan", "pstree"],
+                "Registry": ["hivedump", "hivelist", "hivescan"]}
 
-tree = QTreeWidget()  # add parent here
-tree.setColumnCount(1)
-tree.setHeaderLabels(["Commands"])
+        # add data to the tree
+        commands = []
+        for key, values in data.items():
+            command = QTreeWidgetItem([key])
+            for value in values:
+                ext = value.split(".")[-1]
+                child = QTreeWidgetItem([value, ext])
+                command.addChild(child)
+            commands.append(command)
 
-# onClick function for the commands
-@pyqtSlot(QTreeWidgetItem, int)
-def onItemClick(it, col):
-    match it.text(col):
-        case "pslist":
-            print('psslist case')
-        case "psscan":
-            print('psscan case')
-        case _:
-            print(f"The button {it.text(col)} has been pressed.")
+        tree.insertTopLevelItems(0, commands)
+        tree.itemClicked.connect(self.onItemClick)  # make menu items clickable
 
-# add data to the tree
-commands = []
-for key, values in data.items():
-    command = QTreeWidgetItem([key])
-    for value in values:
-        ext = value.split(".")[-1]
-        child = QTreeWidgetItem([value, ext])
-        command.addChild(child)
-    commands.append(command)
-
-tree.insertTopLevelItems(0, commands)
-tree.itemClicked.connect(onItemClick)  # make menu items clickable
-
-tree.show()
-sys.exit(app.exec())
+    @pyqtSlot(QTreeWidgetItem, int)
+    def onItemClick(self, it, col):
+        match it.text(col):
+            case "pslist":
+                print('psslist case')
+            case "psscan":
+                print('psscan case')
+            case _:
+                print(f"The button {it.text(col)} has been pressed.")
